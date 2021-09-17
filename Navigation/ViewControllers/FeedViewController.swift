cb00b8c595674e000f259142d2f7a21e43018f08
Akmal Astanov <rudy1486@mail.ru>
2021-09-17 17:26:14 +0300

    tasks from 1 to 5 done

---
+++ Navigation/ViewControllers/FeedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by Akmal Astanov on 25.08.2021.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+}
---
+++ Navigation/ViewControllers/PostViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PostViewController.swift
+//  Navigation
+//
+//  Created by Akmal Astanov on 15.09.2021.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Post"
+        view.backgroundColor = .gray
+    }
+    
+
+}
